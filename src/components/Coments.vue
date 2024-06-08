25a56da6e4d40c96b2b506208ff2e9a7eda8b79d
OctoDudeSea <113587395+RobertMihaiStanica@users.noreply.github.com>
2024-06-08 19:19:37 +0200

    Fixed condition for rendering the comments and corrected the paths to the data

--- src/components/Coments.vue
+++ src/components/Coments.vue
@@ -1,22 +1,22 @@
 <template>
-    <div class="coments">
-        <div v-motion-slide-right class="coment" v-if="coments" v-for="(coment, index) in coments" :key="index">
-            <h3>{{ coment.author }}</h3>
-            <p>{{ coment.content }}</p>
+    <div class="comments">
+        <div v-motion-slide-right class="comment" v-if="comments && comments.length > 0" v-for="(comment, index) in comments" :key="index">
+            <h3>{{ comment.author }}</h3>
+            <p>{{ comment.content }}</p>
         </div>
-        <div v-motion-slide-right class="coment" v-else>
-            <h3>No coments yet. Be the first one to comment</h3>
+        <div v-motion-slide-right class="comment" v-else>
+            <h3>No comments yet. Be the first one to comment</h3>
         </div>
     </div>
 </template>
 
 <script> 
-    // Coment structure: { "user": "test", "content": "Test comment"}
+    // comment structure: { "user": "test", "content": "Test comment"}
     export default {
         props: ['id'],
         data() {
             return{
-                coments: null,
+                comments: null,
                 updateComponent: null
             }
         },
@@ -24,7 +24,7 @@
             let  fetchData = () => {
                 fetch("http://localhost:3000/api/note/" + this.id)
                 .then(res => res.json())
-                .then((data => this.coments = data.coments))
+                .then((data => this.comments = data.comments))
                 .catch(error => console.log(error));
 
                 console.log("Fetching data")
@@ -42,18 +42,18 @@
 </script>
 
 <style scoped>
-    .coments {
+    .comments {
         margin: 2em;
     }
 
-    .coment {
+    .comment {
         background-color: rgb(20, 190, 120);
         border-radius: 2em;
         margin-top: 1em;
         padding: 0.5em;
     }
 
-    .coment h3, p{
+    .comment h3, p{
         padding: 0.3em;
         margin: 0;
     }
